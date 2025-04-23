062948fc32149e84f10e0a51722112354b901c46
NITHINKR06 <nithinpoojari606@gmail.com>
2025-04-23 18:38:44 +0530

    added website

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/components/auth/AuthContext";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-import Navbar from '../components/Navbar';
-import AdminNavbar from '../components/AdminNavbar';
 import NavWrapper from '../components/NavWrapper';
 
 export const metadata = {
   title: 'IQues',
-  description: 'Important Questions',
+  description: 'Important Questions ... for your exams',
 }
 
 export default function RootLayout({ children }) {
