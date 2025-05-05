021dab8c86a6ea19010749f5fe74f40614f349a3
NITHINKR06 <nithinpoojari606@gmail.com>
2025-05-05 21:56:36 +0530

    changed the auth

--- src/config/authorizedUsers.js
+++ src/config/authorizedUsers.js
@@ -5,6 +5,9 @@ const authorizedUsers = [
   'user2@iques.com',
   'shravya@nmamit.in',
   'nithin@nmamit.in',
+  'thilak@nmamit.in',
+  'srujan@nmamit.in',
+  'ash@nmamit.in',
   // Add more authorized users as needed
 ];
 
