efbbbd0f40e338b3815b9f46c686f9ba3e0469fa
NITHINKR06 <nithinpoojari606@gmail.com>
2025-05-09 12:33:51 +0530

    Auth page has updated and bit mistakes and error are corrected ..

--- src/config/authorizedUsers.js
+++ src/config/authorizedUsers.js
@@ -1,7 +1,7 @@
 // List of authorized users who can access the website
 const authorizedUsers = [
   'admin@iques.com',
-  'shravya06@nmamit.in',
+  'shravya06',
   'nithin06@nmamit.in',
   'nithin06',
   // Add more authorized users as needed
