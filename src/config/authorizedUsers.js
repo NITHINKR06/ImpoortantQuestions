ac88b6fec723193b9adf30c609b9b3cf3e066d33
NITHINKR06 <nithinpoojari606@gmail.com>
2025-05-11 12:24:16 +0530

    Updated the auth

--- src/config/authorizedUsers.js
+++ src/config/authorizedUsers.js
@@ -1,10 +1,12 @@
 // List of authorized users who can access the website
 const authorizedUsers = [
   'admin@iques.com',
-  'shravya06',
-  'nithin06@nmamit.in',
+  'shravya',
+  'nithin@nmamit.in',
   'nithin06',
   '123321',
+  'thilak',
+  'srujan',
   // Add more authorized users as needed
 ];
 
